3f3a2c2500ff031c9dd984ac996c141c594d5741
RyanD64 <3625@holbertonschool.com>
2022-04-26 13:07:55 +0000

    format 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(F"{i:02d}", end=", ")
     if i == 99:
-        print(F"{i:02d}", end="\n")
+        print("{:d}".format(i), end="\n")
+    else:
+        print("{:d}".format(i), end=", ")
