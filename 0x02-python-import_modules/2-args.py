945cf6d9bed2fc207892295cefe2dbe0a0904e00
RyanD64 <3625@holbertonschool.com>
2022-04-28 11:53:54 +0000

    third file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
-arguments = len(argv)
-index = 1
-if arguments == 0:
-    print(f"{arguments} arguments.")
-elif arguments == 1:
-    print(f"{arguments} argument:")
-    print(f"{arguments}: {sys.argv[1]}")
-else:
-    print(f"{arguments} arguments:")
-    while index <= arguments:
-        print(f"{index}: {sys.argv[index]}")
-        index += 1
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arguments = len(argv)
+    index = 1
+    if arguments == 0:
+        print(f"{arguments} arguments.")
+    elif arguments == 1:
+        print(f"{arguments} argument:")
+        print(f"{arguments}: {sys.argv[1]}")
+    else:
+        print(f"{arguments} arguments:")
+        while index <= arguments:
+            print(f"{index}: {sys.argv[index]}")
+            index += 1
