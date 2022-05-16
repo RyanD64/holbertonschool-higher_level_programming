56f746e1891bfc66e8f61d294c2bc6c92ad4b682
RyanD64 <3625@holbertonschool.com>
2022-05-16 12:20:09 +0000

    third file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     comp = 0
-    miss = 0
     while comp < x:
         try:
             print(f"{my_list[comp]}", end='')
             comp += 1
         except (ValueError, TypeError):
-            comp += 1
-            miss += 1
-    comp -= miss
+            continue
     print()
     return comp
