a276e628ba6ff4195674a1384d98f04134124a32
RyanD64 <3625@holbertonschool.com>
2022-06-01 08:35:40 +0000

    add doc 5

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""return an object represented by
-a json string
-"""
+"""from json string"""
 import json
 
 
 def from_json_string(my_str):
+    """return an object represented by
+    a json string
+    """
     return json.loads(my_str)
