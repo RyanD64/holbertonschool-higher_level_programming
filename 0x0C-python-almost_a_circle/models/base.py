8e7cffd4b65fd254abe0d7a859063c45dcad378a
RyanD64 <3625@holbertonschool.com>
2022-06-07 21:58:15 +0000

    test and filal tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,15 +22,47 @@ class Base:
     def to_json_string(list_dictionaries):
         """return json strings"""
         if type(list_dictionaries) != str or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return the list of the json string representation"""
+        if type(json_string) != str or len(json_string) == 0:
             return []
-        return json.loads(list_dictionaries)
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """"""
+        """write the json representation of list_objs to a file"""
+        with open(cls.__name__ + ".json", mode="w") as write_file:
+            if list_objs is None:
+                write_file.write("[]")
+            else:
+                write_file.write(cls.to_json_string(
+                                 [item.to_dictionary() for item in list_objs]))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return an instance with all atributes already set"""
         if cls.__name__ == "Rectangle":
             tmp = cls(1, 1)
         if cls.__name__ == "Square":
             tmp = cls(1)
-        tmp.update(list_objs)
+        tmp.update(**dictionary)
         return tmp
+
+    @classmethod
+    def load_from_file(cls):
+        """return a list of instances"""
+        res = []
+        with open(cls.__name__ + ".json", mode="r") as read_file:
+            text = read_file.read()
+        text = cls.from_json_string(text)
+        for item in text:
+            if type(item) == dict:
+                res.append(cls.create(**item))
+            else:
+                res.append(item)
+        return
