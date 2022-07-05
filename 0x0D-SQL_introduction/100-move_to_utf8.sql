fc27231c7f4dcc1777b6c6f3c86ca4800df3029d
RyanD64 <3625@holbertonschool.com>
2022-07-05 13:43:02 +0000

    advanced try 4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- converts hbtn_0c_0 database to utf8
-use 'hbtn_0c_0';
-ALTER DATABASE 'hbtn_0c_0' ALTER TABLE 'first table' CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+use hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
