40ef1916ba6490a3e7fa88e8dad10b1494cabbbf
RyanD64 <3625@holbertonschool.com>
2022-07-06 21:02:25 +0000

    correcting file 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of california in the database
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = state_id 
-    AND state.name = 'California'
+WHERE cities.state_id = states_id 
+    AND states.name = 'California'
 ORDER BY cities.id ASC;
