d4e0e13f4c49a274c3e1fac0f485f8f4b9cea9d6
RyanD64 <3625@holbertonschool.com>
2022-08-07 21:35:29 +0000

    try fix 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == '__main__':
     start = MySQLdb.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2],
-                            db=argv[3])
+                            db=argv[3], charset="utf8")
 
     go = start.cursor()
     go.execute("SELECT cities.name FROM cities JOIN states ON \
