f9a15b704d4c3960fde0d2043039541316c1a988
RyanD64 <3625@holbertonschool.com>
2022-09-23 07:09:06 +0000

    correction

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,3 @@
-window.$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-  window.$('DIV#hello').append(data.hello);
+window.$.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
+  window.$('#hello').text(data.hello);
 });
